8d5623631612c5e07e9753ad9f9d22107df40ac0
Tyler Baylson <thbaylson@gmail.com>
2023-04-16 22:49:05 -0400

    Lesson: Enemy State. Added slime enemies. Added enemy AI classes. Organized Sprites folder.

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    private enum State
+    {
+        Roaming
+    }
+
+    private State state;
+    private EnemyPathfinding enemyPathfinding;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        enemyPathfinding = GetComponent<EnemyPathfinding>();
+        state = State.Roaming;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(RoamingRoutine());
+    }
+
+    private IEnumerator RoamingRoutine()
+    {
+        while(state == State.Roaming)
+        {
+            Vector2 roamPosition = GetRoamingPosition();
+            enemyPathfinding.MoveTo(roamPosition);
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+    private Vector2 GetRoamingPosition()
+    {
+        // Normalized to make sure the slime isn't moving faster on diagonals
+        return new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+    }
+}
