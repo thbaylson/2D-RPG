8ff8c7236197ca6760c5081a9b047fb3832eabba
Tyler Baylson <thbaylson@gmail.com>
2023-04-16 21:43:57 -0400

    Lesson: Animating The Player. Added animations to the player. Player faces the direction he moves in.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,11 +9,15 @@ public class PlayerController : MonoBehaviour
     private PlayerControls playerControls;
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Animator myAnimator;
+    private SpriteRenderer myRenderer;
 
     private void Awake()
     {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable()
@@ -24,6 +28,7 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         PlayerInput();
+        AdjustPlayerDirection();
     }
 
     private void FixedUpdate()
@@ -34,11 +39,30 @@ public class PlayerController : MonoBehaviour
     private void PlayerInput()
     {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
     }
 
     private void Move()
     {
         // Multiply floats first to make vector math more computationally efficient
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void AdjustPlayerDirection()
+    {
+        // We only care about updating the position if we're moving
+        if (movement.x != 0)
+        {
+            if (movement.x < 0)
+            {
+                myRenderer.flipX = true;
+            }
+            else if (movement.x > 0)
+            {
+                myRenderer.flipX = false;
+            }
+        }
     }
 }
