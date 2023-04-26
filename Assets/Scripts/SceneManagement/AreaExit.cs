740de22b2ee2a25ea02933c252235c66d0b5542f
Tyler Baylson <thbaylson@gmail.com>
2023-04-26 00:03:31 -0400

    Lesson: Scene Entry. Added logic for moving the player object between scenes. Currently, there is a bug with the camera becoming detached from the player. This will be fixed in the next lesson.

--- Assets/Scripts/SceneManagement/AreaExit.cs
+++ Assets/Scripts/SceneManagement/AreaExit.cs
@@ -6,12 +6,15 @@ using UnityEngine.SceneManagement;
 public class AreaExit : MonoBehaviour
 {
     [SerializeField] private string sceneToLoad;
+    // The name of the entrance we want to appear next to in the next scene.
+    [SerializeField] private string sceneTransitionName;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<PlayerController>())
         {
             SceneManager.LoadScene(sceneToLoad);
+            SceneManagement.Instance.SetTransitionName(sceneTransitionName);
         }
     }
 }
