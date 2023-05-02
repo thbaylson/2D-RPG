2af761c3c8a2ccea0401404aff419b731233fcde
Tyler Baylson <thbaylson@gmail.com>
2023-05-02 02:23:13 -0400

    AimWeapon code cleanup.

--- Assets/Scripts/Inventory/AimWeapon.cs
+++ Assets/Scripts/Inventory/AimWeapon.cs
@@ -6,7 +6,6 @@ public class AimWeapon : MonoBehaviour
 {
     private PlayerControls playerControls;
     private SpriteRenderer playerSpriteRenderer;
-    private Transform weaponCollider;
     
     private Vector2 lookDirection;
 
@@ -22,9 +21,6 @@ public class AimWeapon : MonoBehaviour
 
     private void Start()
     {
-        // This is going to give me an aneurysm. This is clearly a code smell.
-        weaponCollider = PlayerController.Instance.GetWeaponCollider();
-
         playerControls.Movement.Look.performed += ctx => AimInput(ctx.ReadValue<Vector2>());
 
         playerSpriteRenderer = PlayerController.Instance.transform.GetComponent<SpriteRenderer>();
@@ -40,11 +36,6 @@ public class AimWeapon : MonoBehaviour
 
     private void AimForward()
     {
-        // There may be a better way, but I know this way works.
-        //float playerFlipX = playerSpriteRenderer.flipX ? -180f : 0f;
-        //Debug.Log($"Player Flip X: {playerFlipX};");
-        //ActiveWeapon.Instance.transform.rotation = Quaternion.Euler(0f, 0f, playerFlipX);
-        //Debug.Log($"Active Weapon Rotation: {ActiveWeapon.Instance.transform.rotation};");
         float playerFlipX = playerSpriteRenderer.flipX ? -1f : 1f;
         lookDirection = new Vector2(playerFlipX, 0f);
     }
@@ -52,10 +43,9 @@ public class AimWeapon : MonoBehaviour
     private void AimInput(Vector2 controllerInput)
     {
         // This may cause issues on controllers with drift. May need to research deadzone implementation.
-        // This makes sure there is control input, that it isn't a diagonal, (TODO:) and that it isn't pointing behind the player
+        // This makes sure there is controller input and that it isn't a diagonal
         if (!controllerInput.Equals(Vector2.zero) && controllerInput.sqrMagnitude.Equals(1f))
         {
-            //Debug.Log($"Controller Input: {controllerInput};");
             lookDirection = controllerInput;
         }
     }
@@ -69,19 +59,8 @@ public class AimWeapon : MonoBehaviour
             AimForward();
         }
 
-        // If the player is "flipped" on the x axis, then the weapon "rotates" on the y axis
-        float yAxisRotation = playerSpriteRenderer.flipX ? -180f : 0f;
-
-        // Replace Quaternion.Euler's z axis with this and remove yAxisRotation to allow the
-        //  sword to swing around in any direction
-        //Vector2 lookDirection = playerControls.Movement.Look.ReadValue<Vector2>();
-        float controllerAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
-
-
-        // Flip the weapon collider
-        //weaponCollider.transform.rotation = Quaternion.Euler(0f, 0f, controllerAngle);
         // Flip the weapon gameobject
+        float controllerAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
         ActiveWeapon.Instance.transform.rotation = Quaternion.Euler(0f, 0f, controllerAngle);
-        //Debug.Log($"Active Weapon Rotation: {ActiveWeapon.Instance.transform.rotation};");
     }
 }
