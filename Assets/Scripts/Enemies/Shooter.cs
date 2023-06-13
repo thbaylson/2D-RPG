c74cf946ef8c45df1cfd2052a66c469ef91e6d34
Tyler Baylson <thbaylson@gmail.com>
2023-06-12 23:15:18 -0400

    Lesson: Shooter Bursts. Added functionality to Shooter class to allow burst shots.

--- Assets/Scripts/Enemies/Shooter.cs
+++ Assets/Scripts/Enemies/Shooter.cs
@@ -5,12 +5,45 @@ using UnityEngine;
 public class Shooter : MonoBehaviour, IEnemy
 {
     [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private float bulletMoveSpeed;
+    [SerializeField] private int burstCount;
+    [SerializeField] private float timeBetweenBursts;
+    [SerializeField] private float shootCooldown = 1f;
+
+    // TODO: Maybe make this more generic, like "IsAttacking" and add getter to interface?
+    private bool isShooting = false;
 
     public void Attack()
     {
-        Vector2 targetDirection = PlayerController.Instance.transform.position - transform.position;
+        if (!isShooting)
+        {
+            StartCoroutine(ShootRoutine());
+        }
 
-        GameObject newBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
-        newBullet.transform.right = targetDirection;
+    }
+
+    private IEnumerator ShootRoutine()
+    {
+        isShooting = true;
+
+        // Shoot projectiles in defined bursts
+        for(int i = 0; i < burstCount; i++)
+        {
+            Vector2 targetDirection = PlayerController.Instance.transform.position - transform.position;
+
+            GameObject newBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            newBullet.transform.right = targetDirection;
+
+            // Not sure how this is different or better than creating a property and getting the component in start?
+            if (newBullet.TryGetComponent(out Projectile projectile))
+            {
+                projectile.SetProjectileMoveSpeed(bulletMoveSpeed);
+            }
+
+            yield return new WaitForSeconds(timeBetweenBursts);
+        }
+
+        yield return new WaitForSeconds(shootCooldown);
+        isShooting = false;
     }
 }
--- Assets/Scripts/Weapons/Projectile.cs
+++ Assets/Scripts/Weapons/Projectile.cs
@@ -28,6 +28,11 @@ public class Projectile : MonoBehaviour
         this.projectileRange = projectileRange;
     }
 
+    public void SetProjectileMoveSpeed(float moveSpeed)
+    {
+        this.moveSpeed = moveSpeed;
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         EnemyHealth enemyHealth = other.gameObject.GetComponent<EnemyHealth>();
