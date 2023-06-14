c1934144bc90697cfaf6ef83d132193cea47b4c9
Tyler Baylson <thbaylson@gmail.com>
2023-06-14 00:29:32 -0400

    Lesson: Cone Of Influence. Added logic to Shooter to allow projectiles to be shot within a cone of influence.

--- Assets/Scripts/Enemies/Shooter.cs
+++ Assets/Scripts/Enemies/Shooter.cs
@@ -7,6 +7,9 @@ public class Shooter : MonoBehaviour, IEnemy
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private float bulletMoveSpeed;
     [SerializeField] private int burstCount;
+    [SerializeField] private int projectilesPerBurst;
+    [SerializeField][Range(0,359)] private float angleSpread;
+    [SerializeField] private float startingDistance = 0.1f;
     [SerializeField] private float timeBetweenBursts;
     [SerializeField] private float shootCooldown = 1f;
 
@@ -26,24 +29,76 @@ public class Shooter : MonoBehaviour, IEnemy
     {
         isShooting = true;
 
-        // Shoot projectiles in defined bursts
-        for(int i = 0; i < burstCount; i++)
+        float angleStep, startAngle, currentAngle;
+        TargetConeOfInfluence(out angleStep, out startAngle, out currentAngle);
+
+
+        // This represents one complete burst
+        for (int i = 0; i < burstCount; i++)
         {
-            Vector2 targetDirection = PlayerController.Instance.transform.position - transform.position;
-
-            GameObject newBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
-            newBullet.transform.right = targetDirection;
-
-            // Not sure how this is different or better than creating a property and getting the component in start?
-            if (newBullet.TryGetComponent(out Projectile projectile))
+            // This represents the individual bullets in a burst
+            for (int j = 0; j < projectilesPerBurst; j++)
             {
-                projectile.SetProjectileMoveSpeed(bulletMoveSpeed);
+                Vector2 bulletPos = FindBulletSpawnPos(currentAngle);
+
+                GameObject newBullet = Instantiate(bulletPrefab, bulletPos, Quaternion.identity);
+                newBullet.transform.right = newBullet.transform.position - transform.position;
+
+                // Not sure how this is different or better than creating a property and getting the component in start?
+                if (newBullet.TryGetComponent(out Projectile projectile))
+                {
+                    projectile.SetProjectileMoveSpeed(bulletMoveSpeed);
+                }
+
+                // Get the next angle in the cone of influence
+                currentAngle += angleStep;
             }
 
+            // Reset currentAngle between bursts
+            currentAngle = startAngle;
             yield return new WaitForSeconds(timeBetweenBursts);
+
+            // Recalculate cone of influence in case player moves
+            // TODO: Consider if this should stop firing bursts if player is out of attack range
+            TargetConeOfInfluence(out angleStep, out startAngle, out currentAngle);
         }
 
         yield return new WaitForSeconds(shootCooldown);
         isShooting = false;
+    }
+
+    private void TargetConeOfInfluence(out float angleStep, out float startAngle, out float currentAngle)
+    {
+        Vector2 targetDirection = PlayerController.Instance.transform.position - transform.position;
+        // Rad2Deg is a constant that converts radians to degrees. Target angle is the angle (in degrees) of the
+        //  line from this object to the target it's shooting at. It is also the center of the cone of influence.
+        float targetAngle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg;
+
+        angleStep = 0f;
+        float halfAngleSpread = 0f;
+
+        startAngle = targetAngle;
+        float endAngle = targetAngle;
+        currentAngle = targetAngle;
+
+        // In other words, if angle spread IS 0, the object will shoot in a simple straight line
+        if (angleSpread != 0)
+        {
+            angleStep = angleSpread / (projectilesPerBurst - 1);
+            halfAngleSpread = angleSpread / 2f;
+
+            startAngle = targetAngle - halfAngleSpread;
+            endAngle = targetAngle + halfAngleSpread;
+            currentAngle = startAngle;
+        }
+    }
+
+    private Vector2 FindBulletSpawnPos(float currentAngle)
+    {
+        float x = transform.position.x + startingDistance * Mathf.Cos(currentAngle * Mathf.Deg2Rad);
+        float y = transform.position.y + startingDistance * Mathf.Sin(currentAngle * Mathf.Deg2Rad);
+
+        Vector2 pos = new Vector2(x, y);
+        return pos;
     }
 }
