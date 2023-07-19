cc19da3b21155a20f346de1e95b9d0b6d49ff19d
Tyler Baylson <thbaylson@gmail.com>
2023-07-19 12:30:40 -0400

    Lesson: Custom Cursor. Implemented a custom cursor.

---
+++ Assets/Scripts/Misc/CursorManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CursorManager : MonoBehaviour
+{
+    private Image image;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+
+    void Start()
+    {
+        // Hides the OS cursor
+        Cursor.visible = false;
+
+        // Is only true if playing from the editor.
+        if (Application.isEditor)
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        // This will be hit when we actually build/deploy the application.
+        else
+        {
+            // Locks the cursor to the confines of the game window.
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+    }
+
+    void Update()
+    {
+        //Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 cursorPos = Input.mousePosition;
+        image.rectTransform.position = cursorPos;
+
+        // Should probably cut this out before building.
+        if (Application.isEditor) { Cursor.visible = false; }
+    }
+}
