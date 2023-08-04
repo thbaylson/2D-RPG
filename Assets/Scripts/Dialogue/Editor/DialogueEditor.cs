cd28f18c1d02576690c01a1a294cca79c2e00f9d
Tyler Baylson <thbaylson@gmail.com>
2023-08-03 20:27:08 -0400

    Lesson: Opening An Asset Callback. Opening a Dialogue Scriptable Object now opens the DialogueEditor window.

--- Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.Callbacks;
 
 namespace RPG.Dialogue.Editor
 {
@@ -9,6 +10,20 @@ namespace RPG.Dialogue.Editor
         public static void ShowEditorWindow()
         {
             GetWindow(typeof(DialogueEditor), false, "Dialogue Editor", true);
+        }
+
+        [OnOpenAssetAttribute(1)]
+        // This is called whenever any kind of asset is opened.
+        public static bool OnOpenAsset(int instanceID, int line)
+        {
+            // "as" keyword will return null if this is not a Dialogue object.
+            Dialogue dialogue = EditorUtility.InstanceIDToObject(instanceID) as Dialogue;
+            if (dialogue != null)
+            {
+                ShowEditorWindow();
+                return true;
+            }
+            return false;
         }
     }
 }
