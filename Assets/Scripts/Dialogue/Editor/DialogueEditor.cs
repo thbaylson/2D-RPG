270addacb438c4bff944fd739f386e984866559e
Tyler Baylson <thbaylson@gmail.com>
2023-08-03 21:49:34 -0400

    Lesson: Callback Mechanisms. Implemented OnSelectionChanged method and subscibed to it with Selection delegate selectionChanged.

--- Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -6,7 +6,7 @@ namespace RPG.Dialogue.Editor
 {
     public class DialogueEditor : EditorWindow
     {
-        private string[] fruit = { "Apple", "Orange", "Pear" };
+        Dialogue selectedDialogue = null;
 
         [MenuItem("Window/Dialogue Editor")]
         public static void ShowEditorWindow()
@@ -14,7 +14,7 @@ namespace RPG.Dialogue.Editor
             GetWindow(typeof(DialogueEditor), false, "Dialogue Editor", true);
         }
 
-        [OnOpenAssetAttribute(1)]
+        [OnOpenAsset(1)]
         // This is called whenever any kind of asset is opened.
         public static bool OnOpenAsset(int instanceID, int line)
         {
@@ -28,11 +28,34 @@ namespace RPG.Dialogue.Editor
             return false;
         }
 
+        private void OnEnable()
+        {
+            Selection.selectionChanged += OnSelectionChanged;
+        }
+
+        //  We can bypass subscribing to the event altogether, by naming the method
+        //  OnSelectionChange() (no "d") and it will be picked up by the EditorWindow callbacks.
+        private void OnSelectionChanged()
+        {
+            // Try to cast the active object to a Dialogue object
+            Dialogue newDialogue = Selection.activeObject as Dialogue;
+            
+            // Set selectedDialogue or clear it to null if we aren't selecting a Dialogue
+            selectedDialogue = newDialogue;
+
+            // Redraw the GUI
+            Repaint();
+        }
+
         private void OnGUI()
         {
-            foreach(string s in fruit)
+            if(selectedDialogue == null)
             {
-                EditorGUILayout.LabelField(string.Format("{0}", s));
+                EditorGUILayout.LabelField("No Dialogue Selected.");
+            }
+            else
+            {
+                EditorGUILayout.LabelField(selectedDialogue.name);
             }
         }
     }
